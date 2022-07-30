3102a431fbe6fba842db0b0f02539844c9a9509e
thutra1999 <trathutran9@gmail.com>
2022-07-31 01:21:32 +0700

    day 1

---
+++ blog-demo/src/App.js
@@ -0,0 +1,27 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    //Header
+<nav class="navbar navbar-light bg-light">
+  <form class="container-fluid">
+    <div class="input-group">
+      <span class="input-group-text" id="basic-addon1"><i class="fa fa-solid fa-cloud"></i></span>
+      <input type="text" class="form-control" placeholder="Username" aria-label="Username" aria-describedby="basic-addon1"/>
+    </div>
+  </form>
+</nav>
+    //End Of Header
+
+    //Content
+
+    //End Of Content
+
+    //Footer
+
+    //End Of Footer
+  );
+}
+
+export default App;
